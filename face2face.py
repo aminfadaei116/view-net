e3fd4eb6d27462393810c8bd0940524dd282d115
Amin Fadaeinejad <51967292+aminfadaei116@users.noreply.github.com>
2023-01-23 12:41:38 -0500

    Update face2face.py

--- face2face.py
+++ face2face.py
@@ -1,29 +1,10 @@
 import numpy as np
-import mediapipe as mp
-from numpy import save
-import math as m
-import matplotlib
-import matplotlib.pyplot as plt
-import os
-from scipy.io import savemat
-import scipy.io
-import matplotlib.image as mpimg
-import matplotlib.pyplot as plt
-from scipy import signal
-from scipy import stats
-from numpy import moveaxis
-from numpy import asarray
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
 import torchvision
-from torch.linalg import multi_dot
 import configs.config as config
 from utils.util import *
 from matplotlib import cm
-from torch.utils.data import Dataset
 from torchvision import datasets
-from torchvision.transforms import ToTensor
 import torch.optim as optim
 from models.image2image import RenderImage
 from utils.test import UseWebcam
