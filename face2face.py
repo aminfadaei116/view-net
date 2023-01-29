80086c753ea8fb804417cf58f0317c171fb613b7
Amin Fadaeinejad <51967292+aminfadaei116@users.noreply.github.com>
2023-01-29 16:37:36 -0500

    changed variable names

--- face2face.py
+++ face2face.py
@@ -4,30 +4,21 @@ import torchvision
 from configs.config import Config
 from utils.util import *
 from utils.test import UseWebcam
-# from models.generator_model import GeneratorUNet
-# from models.discriminator_model import Discriminator
-# from data.face2face_dataset import ImageDataset
 
-# import os
-# from pix2pix.options.test_options import TestOptions
-# from pix2pix.data import create_dataset
-# from pix2pix.models import create_model
-# from pix2pix.util.visualizer import save_images
-# from pix2pix.util import html
 
 def main():
 
     config = Config("YorkU")
-    imgRef = torchvision.io.read_image(config.PathImg1)
+    img_ref = torchvision.io.read_image(config.PathImg1)
     # imgTar = torchvision.io.read_image(config.PathImg2)
 
-    refKey = torch.tensor(np.load(config.PathNPY1), device=config.DEVICE)
+    ref_key = torch.tensor(np.load(config.PathNPY1), device=config.DEVICE)
     # tarKey = torch.tensor(np.load(config.PathNPY2), device=config.DEVICE)
 
-    height, width = imgRef.shape[1], imgRef.shape[2]
+    height, width = img_ref.shape[1], img_ref.shape[2]
     print("We are using the: ", config.DEVICE)
 
-    UseWebcam(config, height, width, refKey, imgRef)
+    UseWebcam(config, height, width, ref_key, img_ref)
 
 
 if __name__ == "__main__":
--- utils/util.py
+++ utils/util.py
@@ -6,154 +6,103 @@ This script contains the functions of our model
 
 @author: Amin Fadaeinejad
 """
-import numpy as np
-from numpy import save
-import math as m
 import matplotlib.pyplot as plt
-import torch.nn as nn
-import torch.nn.functional as F
-# from configs.config import Config
 import torch
-import cv2
 
 
-def weights_init_normal(m):
-    classname = m.__class__.__name__
-    if classname.find("Conv") != -1:
-        torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
-    elif classname.find("BatchNorm2d") != -1:
-        torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
-        torch.nn.init.constant_(m.bias.data, 0.0)
-        
-
-def createMask(config, keys, height, width, img=None):
-    
-    if img != None:
-      newImg = img.clone().detach().to(config.DEVICE)
-      newImg[:, (height*keys[:, 1]).long(), (width*keys[:, 0]).long()] = 255.0
-      return newImg
-    else: 
-      mask = torch.zeros((height, width), device=config.DEVICE)
-      mask[(height*keys[:, 1]).long(), (width*keys[:, 0]).long()] = 1
-      return mask
+def create_mask(config, keys, height, width, img=None):
+    if img is not None:
+        new_img = img.clone().detach().to(config.DEVICE)
+        new_img[:, (height * keys[:, 1]).long(), (width * keys[:, 0]).long()] = 255.0
+        return new_img
+    else:
+        mask = torch.zeros((height, width), device=config.DEVICE)
+        mask[(height * keys[:, 1]).long(), (width * keys[:, 0]).long()] = 1
+        return mask
 
 
-def createMask2(config, keys, height, width, img=None):
-    
-    if img != None:
-      newImg = img.clone().detach().to(config.DEVICE)
-      for i in range(-1, 2):
-        for j in range(-1, 2):
-          newImg[:, (height*keys[:, 1]).long()+i, (width*keys[:, 0]).long()+j] = 255.0
-      return newImg
-    else: 
-      mask = torch.zeros((height, width), device=config.DEVICE)
-      for i in range(-1, 2):
-        for j in range(-1, 2):
-          mask[(height*keys[:, 1]).long()+i, (width*keys[:, 0]).long()+j] = 1
-      return mask
+def create_mask2(config, keys, height, width, img=None):
+    if img is not None:
+        new_img = img.clone().detach().to(config.DEVICE)
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                new_img[:, (height * keys[:, 1]).long() + i, (width * keys[:, 0]).long() + j] = 255.0
+        return new_img
+    else:
+        mask = torch.zeros((height, width), device=config.DEVICE)
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                mask[(height * keys[:, 1]).long() + i, (width * keys[:, 0]).long() + j] = 1
+        return mask
 
-def showImageTensor(img, is3chan=True, isOutput=False, returnOutput=False):
-  newImg = img.clone().detach()
-  if isOutput:
-    newImg = torch.squeeze(newImg)
 
-  newImg = newImg.to('cpu') 
-  if is3chan:
-    # plt.imshow((newImg.permute(1, 2, 0)*255).int())
-    plt.imshow((newImg.permute(1, 2, 0)))
+def show_image_tensor(img, is3chan=True, is_output=False, return_output=False):
+    new_img = img.clone().detach()
+    if is_output:
+        new_img = torch.squeeze(new_img)
+
+    new_img = new_img.to('cpu')
+    if is3chan:
+        # plt.imshow((newImg.permute(1, 2, 0)*255).int())
+        plt.imshow((new_img.permute(1, 2, 0)))
+        plt.xlabel("x axis")
+        plt.ylabel("y axis")
+    else:
+        plt.imshow(new_img)
+        plt.xlabel("x axis")
+        plt.ylabel("y axis")
+    if return_output:
+        return new_img
+
+
+def draw(width, height, ref_key, tar_key, size=10, connect=False):
+    key1 = torch.zeros((len(ref_key), 2))
+    key2 = torch.zeros((len(tar_key), 2))
+    key1[:, 0] = ref_key[:, 0]
+    key1[:, 1] = tar_key[:, 0]
+
+    key2[:, 0] = ref_key[:, 1]
+    key2[:, 1] = tar_key[:, 1]
+
+    if connect:
+        for i in range(len(key1)):
+            plt.plot(key1[i].cpu().numpy() * width, key2[i].cpu().numpy() * height)
+
+    plt.scatter(ref_key[:, 0].cpu().numpy() * width, ref_key[:, 1].cpu().numpy() * height, s=size)
+    plt.scatter(tar_key[:, 0].cpu().numpy() * width, tar_key[:, 1].cpu().numpy() * height, s=size)
+
+    plt.xlim(0, width - 1)
+    plt.ylim(0, height - 1)
     plt.xlabel("x axis")
     plt.ylabel("y axis")
-  else:
-    plt.imshow(newImg)
-    plt.xlabel("x axis")
-    plt.ylabel("y axis")
-  if returnOutput:
-    return newImg
+
+    ax = plt.gca()
+    ax.set_aspect('equal', adjustable='box')
+    plt.gca().invert_yaxis()
+    plt.show()
 
 
-def draw(width, height, refKey, tarKey, size=10, connect=False):
-
-  key1 = torch.zeros((len(refKey), 2))
-  key2 = torch.zeros((len(tarKey), 2))
-  key1[:, 0] = refKey[:, 0]
-  key1[:, 1] = tarKey[:, 0]
-
-  key2[:, 0] = refKey[:, 1]
-  key2[:, 1] = tarKey[:, 1]
-
-
-  if connect:
-    for i in range(len(key1)):
-        plt.plot(key1[i].cpu().numpy()*width, key2[i].cpu().numpy()*height)
-      
-  plt.scatter(refKey[:,0].cpu().numpy()*width, refKey[:,1].cpu().numpy()*height, s=size)
-  plt.scatter(tarKey[:,0].cpu().numpy()*width, tarKey[:,1].cpu().numpy()*height, s=size)
-
-  plt.xlim(0, width-1)
-  plt.ylim(0, height-1)
-  plt.xlabel("x axis")
-  plt.ylabel("y axis")
-
-  ax = plt.gca()
-  ax.set_aspect('equal', adjustable='box')
-  plt.gca().invert_yaxis()
-  plt.show()
-
-
-def TransformKeys(keys, euler, T):
-    R = torch.linalg.multi_dot((Rx(euler[0]), Ry(euler[1]), Rz(euler[2])))
+def transform_keys(keys, euler, t):
+    R = torch.linalg.multi_dot((R_x(euler[0]), R_y(euler[1]), R_z(euler[2])))
     center = torch.mean(keys, dim=0)
     keys = keys - center
     out = torch.matmul(keys, R)
-    return out + center + T
-    
-def Rx(config, theta):
-    return torch.tensor([[ 1, 0           , 0           ],
-                   [ 0, torch.cos(theta),-torch.sin(theta)],
-                   [ 0, torch.sin(theta), torch.cos(theta)]], device=config.DEVICE, dtype=torch.double)
-  
-def Ry(config, theta):
-    return torch.tensor([[ torch.cos(theta), 0, torch.sin(theta)],
-                   [ 0           , 1, 0           ],
-                   [-torch.sin(theta), 0, torch.cos(theta)]], device=config.DEVICE, dtype=torch.double)
-  
-def Rz(config, theta):
-    return torch.tensor([[ torch.cos(theta), -torch.sin(theta), 0 ],
-                   [ torch.sin(theta), torch.cos(theta) , 0 ],
-                   [ 0           , 0            , 1 ]], device=config.DEVICE, dtype=torch.double)
+    return out + center + t
 
 
-# class Feature2Feature(nn.Module):
-#     def __init__(self, first_layer, last_layer):
-#         super(Feature2Feature, self).__init__()
-#         self.layers = nn.Sequential(
-#             nn.Conv2d(in_channels=first_layer, out_channels=8,kernel_size=3, padding=1),
-#             nn.ReLU(),
-#             nn.Conv2d(in_channels=8, out_channels=8,kernel_size=3, padding=1),
-#             nn.ReLU(),
-#             nn.Conv2d(in_channels=8, out_channels=8,kernel_size=3, padding=1),
-#             nn.ReLU(),
-#             nn.Conv2d(in_channels=8, out_channels=8,kernel_size=3, padding=1),
-#             nn.ReLU(),
-#             nn.Conv2d(in_channels=8, out_channels=8,kernel_size=3, padding=1),
-#             nn.ReLU(),
-#             nn.Conv2d(in_channels=8, out_channels=last_layer,kernel_size=3, padding=1),
-#         )
-#         self.id = nn.Identity()
+def R_x(config, theta):
+    return torch.tensor([[1, 0, 0],
+                         [0, torch.cos(theta), -torch.sin(theta)],
+                         [0, torch.sin(theta), torch.cos(theta)]], device=config.DEVICE, dtype=torch.double)
 
-#     def forward(self, x):
-#         return self.layers(x)
 
-# class ViewNet(nn.Module):
-#     def __init__(self):
-#         super(ViewNet, self).__init__()
-#         self.Image2Feature = Feature2Feature(3, 8)
-#         self.Feature2Image = Feature2Feature(8, 3)
+def R_y(config, theta):
+    return torch.tensor([[torch.cos(theta), 0, torch.sin(theta)],
+                         [0, 1, 0],
+                         [-torch.sin(theta), 0, torch.cos(theta)]], device=config.DEVICE, dtype=torch.double)
 
-#     def forward(self, height, width, refKey, tarKey, x, sd=0.01):
-#         Features = self.Image2Feature(x)
-#         TransformedFearures = RenderImage(height, width, refKey, tarKey, Features, sd=0.01, numChannel=8)
-#         Target = self.Feature2Image(TransformedFearures)
-#         return Target
-        
+
+def R_z(config, theta):
+    return torch.tensor([[torch.cos(theta), -torch.sin(theta), 0],
+                         [torch.sin(theta), torch.cos(theta), 0],
+                         [0, 0, 1]], device=config.DEVICE, dtype=torch.double)
