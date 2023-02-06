9c04952cf27fba6dc3dc4dfadc460f8595cf12fa
Amin Fadaeinejad <51967292+aminfadaei116@users.noreply.github.com>
2023-02-06 15:39:51 -0500

    editting

--- options/face2face_options.py
+++ options/face2face_options.py
@@ -5,17 +5,37 @@ This script contains the options used doe the face2face model
 @author: Amin Fadaeinejad
 """
 import argparse
+from .base_options import BaseOptions
+from options.test_options import TestOptions
+from options.train_options import TrainOptions
 
 
-class Face2FaceOption:
+class Face2FaceOption(BaseOptions):
     """
     This class defines options used for the face2face model
     """
     def __init__(self) -> None:
+        """
+        The first three are for the face2face model itself
+        """
         parser = argparse.ArgumentParser(description='Gather the parameters for the project')
         parser.add_argument('--used_device', type=str, default='none',
                             help='Which device are you using at the moment-> '
                                  '[ YorkU | Ubisoft | laptop ]')
+        parser.add_argument('--mode', type=str, default='test', help="Model mode to -> [ train | test ] ")
+        """
+        The default base options for the pix2pix model
+        """
+        # parser = BaseOptions.initialize(self, parser)
+        """
+        The options for test or tran mode
+        """
+        if parser.parse_args().mode == "train":
+            parser = TrainOptions.initialize(self, parser)
+        elif parser.parse_args().mode == "test":
+            parser = TestOptions.initialize(self, parser)
+        else:
+            raise Exception("Mode not valid")
         self.parser = parser
 
     def get_parser(self):
